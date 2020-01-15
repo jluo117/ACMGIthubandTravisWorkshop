e263e268f7c7c9ebb5374a6d77621757a375b5d2
james luo <luojames52@gmail.com>
2020-01-14 22:55:50 -0800

    testing

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
-from fib import Fibonacci
+from fib import Fibonacci , fib_fast
 import unittest
 print(Fibonacci(10))
 class TestStringMethods(unittest.TestCase):
     def test_Fib(self):
         self.assertEqual(Fibonacci(10),34)
-    def test_bigNum(self):
-        self.assertEqual(Fibonacci(20),4181)
+        self.assertEqual(Fibonacci(30),514229)
+        self.assertEqual(Fibonacci(35),5702887)
+    def test_fastFib(self):
+        self.assertEqual(fib_fast(20),4181)
+
 if __name__ == '__main__':
     unittest.main()
